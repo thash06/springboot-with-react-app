d2c17a027d2db64fd9896bf8cc56f54c89841753
aseem <shrestha.asm@gmail.com>
2019-04-23 17:38:56 -0400

    move css to css folder and update readme file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
 
 class App extends Component {
 
@@ -26,6 +26,7 @@ class App extends Component {
                     <h1 className="App-title">{this.state.message}</h1>
                 </header>
                 <p className="App-intro">
+                    <h1>Welcome to React!</h1>
                 </p>
             </div>
         );
