3e407f55be67e9178e979cb506d327ee24a31b1c
Weng Zhuo <wenzhuo@publicisgroupe.net>
2019-06-11 17:06:11 -0400

    unregister serverwork

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
 
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+serviceWorker.unregister();
